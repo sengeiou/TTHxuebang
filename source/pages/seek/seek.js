43b70dc924cf3eaf3c6e97740b5fbc1813d1a558
alucard263096 <alucard263096@126.com>
2019-03-29 15:21:57 +0800

    Commit

--- source/pages/seek/seek.js
+++ source/pages/seek/seek.js
@@ -1,8 +1,19 @@
 // pages/seek/seek.js
-import { AppBase } from "../../appbase";
-import { ApiConfig } from "../../apis/apiconfig";
-import { InstApi } from "../../apis/inst.api.js";
-import { JigouApi } from "../../apis/jigou.api.js";
+import {
+  AppBase
+} from "../../appbase";
+import {
+  ApiConfig
+} from "../../apis/apiconfig";
+import {
+  ApiUtil
+} from "../../apis/apiutil";
+import {
+  InstApi
+} from "../../apis/inst.api.js";
+import {
+  JigouApi
+} from "../../apis/jigou.api.js";
 
 class Content extends AppBase {
   constructor() {
@@ -15,30 +26,41 @@ class Content extends AppBase {
     this.Base.setMyData({
       type: this.options.type,
       //type: "kc",
-      show:"jx",
-      options:"j_x",
+      show: "jx",
+      options: "j_x",
+      mylat: 0,
+      mylng: 0,
       options_show: false
     })
-  //  console.log(this.options.type);
+    //  console.log(this.options.type);
   }
   onMyShow() {
     var that = this;
     var instapi = new InstApi();
-    var jigouapi = new JigouApi();
-    var show=this.Base.getMyData().show;
-    instapi.indexbanner({}, (indexbanner) => {
-      this.Base.setMyData({ indexbanner });
-    });
-    if(show=='jx'){
-      jigouapi.jglist({ id: 1 }, (jglist) => {
-        this.Base.setMyData({ jglist });
-      });
-    }
+    var show = this.Base.getMyData().show;
 
-    jigouapi.courselist({ }, (courselist) => {
-      this.Base.setMyData({ courselist });
-    });
+
+
+    console.log(show);
+
     
+      this.Base.getAddress((address) => {
+        console.log(address);
+        var mylat = address.location.lat;
+        var mylng = address.location.lng;
+        console.log("mylat");
+        this.Base.setMyData({
+          mylat,
+          mylng
+        });
+        if(show=="jx"){
+
+          this.loadjg();
+        }
+        this.loadcourse();
+      });
+
+
   }
   tojgdetails(e) {
     var id = e.currentTarget.id;
@@ -47,74 +69,148 @@ class Content extends AppBase {
     })
   }
 
-  tokcdetails(e){
+  tokcdetails(e) {
     var id = e.currentTarget.id;
     wx.navigateTo({
       url: '/pages/kcdetails/kcdetails?id=' + id,
     })
   }
-  
-  bindshow(e){
-    var type=e.currentTarget.dataset.type;
+
+  bindshow(e) {
+    var type = e.currentTarget.dataset.type;
     console.log(type);
-    if(type=="jx"){
-      this.Base.setMyData({ show: "jx" })
+    if (type == "jx") {
+      this.Base.setMyData({
+        show: "jx"
+      })
       var jigouapi = new JigouApi();
-       jigouapi.jglist({  }, (jglist) => {
-         this.Base.setMyData({ jglist });
-       });
+      jigouapi.jglist({}, (jglist) => {
+        this.Base.setMyData({
+          jglist
+        });
+      });
     }
     if (type == "xs") {
-      this.Base.setMyData({ show: "xs" })
+      this.Base.setMyData({
+        show: "xs"
+      })
       var jigouapi = new JigouApi();
-      jigouapi.jglist({ jigou: "测试机构二" }, (jglist) => {
-        this.Base.setMyData({ jglist });
+      jigouapi.jglist({
+        jigou: "测试机构二"
+      }, (jglist) => {
+        this.Base.setMyData({
+          jglist
+        });
       });
 
     }
     if (type == "hp") {
-      this.Base.setMyData({ show: "hp" })
+      this.Base.setMyData({
+        show: "hp"
+      })
       var jigouapi = new JigouApi();
-      jigouapi.jglist({ jigou: "测试机构一" }, (jglist) => {
-        this.Base.setMyData({ jglist });
+      jigouapi.jglist({
+        jigou: "测试机构一"
+      }, (jglist) => {
+        this.Base.setMyData({
+          jglist
+        });
       });
     }
 
   }
 
-  bindxuanxiang(e){
+  bindxuanxiang(e) {
     var options = e.currentTarget.dataset.options;
     console.log(options);
     if (options == "j_x") {
-      this.Base.setMyData({ options: "j_x" })
-      
-      
+      this.Base.setMyData({
+        options: "j_x"
+      })
+
+
     }
     if (options == "x_s") {
-      this.Base.setMyData({ options: "x_s" })
-     
+      this.Base.setMyData({
+        options: "x_s"
+      })
+
 
     }
     if (options == "bm_za") {
-      this.Base.setMyData({ options: "bm_za" })
+      this.Base.setMyData({
+        options: "bm_za"
+      })
 
 
     }
     if (options == "h_p") {
-      this.Base.setMyData({ options: "h_p" })
-      
+      this.Base.setMyData({
+        options: "h_p"
+      })
+
     }
   }
 
-  bindScreening(e){
+  bindScreening(e) {
     var qd = e.currentTarget.dataset.qd;
-    if (qd=="ok"){
-      this.Base.setMyData({  options_show: false })
+    if (qd == "ok") {
+      this.Base.setMyData({
+        options_show: false
+      })
+    } else {
+      this.Base.setMyData({
+        options: "s_x",
+        options_show: true
+      })
     }
-    else{
-      this.Base.setMyData({ options: "s_x", options_show: true })
-    }
-    
+
+  }
+  loadjg() {
+    var jigouapi = new JigouApi();
+    var mylat = this.Base.getMyData().mylat;
+    var mylng = this.Base.getMyData().mylng;
+    jigouapi.jglist({
+      mylat,
+      mylng,
+      orderby: "distance"
+    }, (jglist) => {
+      for (var i = 0; i < jglist.length; i++) {
+        console.log(jglist[i]);
+        var mile = ApiUtil.GetDistance(mylat, mylng, jglist[i].lat, jglist[i].lng);
+        console.log("mile=" + mile);
+        var miletxt = ApiUtil.GetMileTxt(mile);
+        console.log("miletxt=" + miletxt);
+        jglist[i]["miletxt"] = miletxt;
+      }
+      this.Base.setMyData({
+        jglist
+      });
+    });
+  }
+  loadcourse() {
+    var jigouapi = new JigouApi();
+    var mylat = this.Base.getMyData().mylat;
+    var mylng = this.Base.getMyData().mylng;
+    jigouapi.courselist({
+      mylat,
+      mylng,
+      orderby: "distance"
+    }, (courselist) => {
+      for (var i = 0; i < courselist.length; i++) {
+        var mile = ApiUtil.GetDistance(mylat, mylng, courselist[i].JG_lat, courselist[i].JG_lng);
+        console.log("mile=" + mile);
+        var miletxt = ApiUtil.GetMileTxt(mile);
+        console.log("miletxt=" + miletxt);
+        courselist[i]["miletxt"] = miletxt;
+      }
+      this.Base.setMyData({
+        courselist
+      });
+    });
+  }
+  hideFilter(){
+    this.Base.setMyData({ options_show:false});
   }
 }
 
@@ -124,8 +220,11 @@ body.onLoad = content.onLoad;
 body.onMyShow = content.onMyShow;
 body.tojgdetails = content.tojgdetails;
 body.tokcdetails = content.tokcdetails;
-body.bindxuanxiang = content.bindxuanxiang; 
+body.bindxuanxiang = content.bindxuanxiang;
 
 body.bindScreening = content.bindScreening;
 body.bindshow = content.bindshow;
+body.loadjg = content.loadjg; 
+body.loadcourse = content.loadcourse;
+body.hideFilter = content.hideFilter;
 Page(body)
