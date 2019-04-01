deb47c5d473e94326f8db9cf5274475040da670e
alucard263096 <alucard263096@126.com>
2019-04-01 18:15:31 +0800

    commit

--- source/apis/jigou.api.js
+++ source/apis/jigou.api.js
@@ -360,4 +360,36 @@ export class JigouApi{
             }
         })
     }
+
+    checkcanbuy(json, callback, showLoading = true) {
+
+        if (showLoading)
+            ApiConfig.ShowLoading();
+
+        var header = ApiConfig.GetHeader();
+        console.log(header);
+        console.log(json);
+        wx.request({
+            url: ApiConfig.GetApiUrl() + 'jigou/checkcanbuy',
+            data: json,
+            method: 'POST',
+            dataType: 'json',
+            header: header,
+            success: function (res) {
+                if (callback != null) {
+                    callback(res.data);
+                }
+            },
+            fail: function (res) {
+                console.log(res);
+                callback(false);
+            },
+            complete: function (res) {
+                console.log(res);
+            
+                if (showLoading)
+                    ApiConfig.CloseLoading();
+            }
+        })
+    }
 }
