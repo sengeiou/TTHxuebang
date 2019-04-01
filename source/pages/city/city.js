48110a0f0d4c70177549b51e4fdc57a7c4460be7
Vvenom <tigerhi@qq.com>
2019-04-01 14:47:07 +0800

    aaa

---
+++ source/pages/city/city.js
@@ -0,0 +1,32 @@
+// pages/city/city.js
+import { AppBase } from "../../appbase";
+import { ApiConfig } from "../../apis/apiconfig";
+import { InstApi } from "../../apis/inst.api.js";
+import { JigouApi } from "../../apis/jigou.api.js";
+
+class Content extends AppBase {
+  constructor() {
+    super();
+  }
+
+  onLoad(options) {
+    this.Base.Page = this;
+    //options.id=5;
+    super.onLoad(options);
+  }
+
+  onMyShow() {
+    var that = this;
+    var instapi = new InstApi();
+    instapi.indexbanner({}, (indexbanner) => {
+      this.Base.setMyData({ indexbanner });
+    });
+  }
+
+}
+
+var content = new Content();
+var body = content.generateBodyJson();
+body.onLoad = content.onLoad;
+body.onMyShow = content.onMyShow;
+Page(body)
