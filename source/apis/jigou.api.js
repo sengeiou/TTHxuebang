9c1bc1fa93580874a527cac3f144e4c7aaa4bd03
992613643 <992613643@qq.com>
2019-07-22 20:54:50 +0800

    qweqwe

--- source/apis/jigou.api.js
+++ source/apis/jigou.api.js
@@ -1417,6 +1417,38 @@ export class JigouApi{
         })
     }
 
+    shanchuxiaoxi(json, callback, showLoading = true) {
+
+        if (showLoading)
+            ApiConfig.ShowLoading();
+
+        var header = ApiConfig.GetHeader();
+        console.log(header);
+        console.log(json);
+        wx.request({
+            url: ApiConfig.GetApiUrl() + 'jigou/shanchuxiaoxi',
+            data: json,
+            method: 'POST',
+            dataType: 'json',
+            header: header,
+            success: function (res) {
+                if (callback != null) {
+                    callback(res.data);
+                }
+            },
+            fail: function (res) {
+                console.log(res);
+                callback(false);
+            },
+            complete: function (res) {
+                console.log(res);
+            
+                if (showLoading)
+                    ApiConfig.CloseLoading();
+            }
+        })
+    }
+
     shanchuxueyuan(json, callback, showLoading = true) {
 
         if (showLoading)
@@ -1833,7 +1865,7 @@ export class JigouApi{
         })
     }
 
-    shanchuxiaoxi(json, callback, showLoading = true) {
+    xiaoxiinfo(json, callback, showLoading = true) {
 
         if (showLoading)
             ApiConfig.ShowLoading();
@@ -1842,7 +1874,7 @@ export class JigouApi{
         console.log(header);
         console.log(json);
         wx.request({
-            url: ApiConfig.GetApiUrl() + 'jigou/shanchuxiaoxi',
+            url: ApiConfig.GetApiUrl() + 'jigou/xiaoxiinfo',
             data: json,
             method: 'POST',
             dataType: 'json',
--- source/pages/mymessage/mymessage.js
+++ source/pages/mymessage/mymessage.js
@@ -14,23 +14,7 @@ class Content extends AppBase {
     //options.id=5;
     super.onLoad(options);
 
-    this.Base.setMyData({
-      right: [{
-        text: '取消',
-        style: 'background-color: #ddd; color: #fff;padding-left:20rpx;padding-right:20rpx',
-      },
-      {
-        text: '删除',
-        style: 'background-color: #F4333C; color: #fff;padding-left:20rpx;padding-right:20rpx',
-      }],
-      left: [{
-        text: 'Reply',
-        style: 'background-color: #108ee9; color: white',
-      },
-      {
-        text: 'Cancel',
-        style: 'background-color: #ddd; color: white',
-      }],})
+
   }
   onMyShow() {
     var that = this;
@@ -38,33 +22,82 @@ class Content extends AppBase {
     var jigouapi = new JigouApi();
 
     var api = new PurchaseApi();
-   
-    jigouapi.myxiaoxi({},(xiaoxilist)=>{
 
-      this.Base.setMyData({ xiaoxilist: xiaoxilist})
-  console.log(12132);
+    jigouapi.myxiaoxi({}, (xiaoxilist) => {
+      for (var i = 0; i < xiaoxilist.length; i++) {
+        xiaoxilist[i].youhua = [{
+          text: '取消',
+
+          style: 'background-color: #ddd; color: #fff;padding-left:20rpx;padding-right:20rpx',
+        },
+        {
+          text: '删除',
+          id: xiaoxilist[i].id,
+          style: 'background-color: #F4333C; color: #fff;padding-left:20rpx;padding-right:20rpx',
+        }]
+
+      }
+
+      this.Base.setMyData({ xiaoxilist: xiaoxilist })
+      console.log(12132);
     })
-  
+
     instapi.indexbanner({}, (indexbanner) => {
       this.Base.setMyData({ indexbanner });
     });
     jigouapi.jglist({}, (jglist) => {
       this.Base.setMyData({ jglist });
     });
-
-
-
-
-
-    
   }
 
 
   onClick(e) {
     console.log('onClick', e.detail)
+    if (e.detail.index == 0) {
+      return
+    }
+    var xiaoxilist = this.Base.getMyData().xiaoxilist;
+    xiaoxilist = xiaoxilist.filter((item) => {
+
+      return item.id != e.detail.value.id
+
+    })
+    this.Base.setMyData({ xiaoxilist })
+
+     var api=new JigouApi();
+    api.shanchuxiaoxi({ id: e.detail.value.id},(res)=>{
+    
+    console.log("真的删除了啊啊");
+    console.log(res);
+
+    })
+
+
   }
   onShare() {
     console.log('onShare')
+  }
+  xianqin(e) {
+    console.log(e);
+    var xiaoxilist=this.Base.getMyData().xiaoxilist;
+       xiaoxilist=xiaoxilist.filter((item)=>{
+       
+         return item.id = e.currentTarget.dataset.id;
+
+       })
+       if(xiaoxilist[0].type=='A')
+       {
+
+    wx.navigateTo({
+      url: '/pages/jiaoyixinxi/jiaoyixinxi?id=' + e.currentTarget.dataset.id,
+    })
+       }
+    if (xiaoxilist[0].type == 'C') {
+
+      wx.navigateTo({
+        url: '/pages/tuiguanxiaoxi/tuiguanxiaoxi?id=' + e.currentTarget.dataset.id,
+      })
+    }
   }
 }
 
@@ -72,7 +105,8 @@ var content = new Content();
 var body = content.generateBodyJson();
 body.onLoad = content.onLoad;
 body.onMyShow = content.onMyShow;
-body.tojgdetails = content.tojgdetails; 
+body.tojgdetails = content.tojgdetails;
 body.onShare = content.onShare;
 body.onClick = content.onClick;
+body.xianqin = content.xianqin;
 Page(body)
