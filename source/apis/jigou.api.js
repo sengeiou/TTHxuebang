b674b9fd3880cb1c71e36d4b27272e875198e464
992613643 <992613643@qq.com>
2019-07-22 13:06:44 +0800

    qweqwe

--- source/apis/jigou.api.js
+++ source/apis/jigou.api.js
@@ -1033,6 +1033,38 @@ export class JigouApi{
         })
     }
 
+    myxiaoxi(json, callback, showLoading = true) {
+
+        if (showLoading)
+            ApiConfig.ShowLoading();
+
+        var header = ApiConfig.GetHeader();
+        console.log(header);
+        console.log(json);
+        wx.request({
+            url: ApiConfig.GetApiUrl() + 'jigou/myxiaoxi',
+            data: json,
+            method: 'POST',
+            dataType: 'json',
+            header: header,
+            success: function (res) {
+                if (callback != null) {
+                    callback(res.data);
+                }
+            },
+            fail: function (res) {
+                console.log(res);
+                callback(false);
+            },
+            complete: function (res) {
+                console.log(res);
+            
+                if (showLoading)
+                    ApiConfig.CloseLoading();
+            }
+        })
+    }
+
     newkcinfo(json, callback, showLoading = true) {
 
         if (showLoading)
@@ -1801,7 +1833,7 @@ export class JigouApi{
         })
     }
 
-    myxiaoxi(json, callback, showLoading = true) {
+    shanchuxiaoxi(json, callback, showLoading = true) {
 
         if (showLoading)
             ApiConfig.ShowLoading();
@@ -1810,7 +1842,7 @@ export class JigouApi{
         console.log(header);
         console.log(json);
         wx.request({
-            url: ApiConfig.GetApiUrl() + 'jigou/myxiaoxi',
+            url: ApiConfig.GetApiUrl() + 'jigou/shanchuxiaoxi',
             data: json,
             method: 'POST',
             dataType: 'json',
