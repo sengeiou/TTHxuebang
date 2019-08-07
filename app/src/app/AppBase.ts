7b239b2376bed9baa429ead2879c71234cc16539
alucard263096 <alucard263096@126.com>
2019-08-07 16:32:03 +0800

    commit

--- app/src/app/AppBase.ts
+++ app/src/app/AppBase.ts
@@ -359,22 +359,22 @@ export class AppBase implements OnInit {
             desc=this.InstInfo.h5sharedesc;
         }
         AppBase.wechatApi.gensign({url:window.location.href}).then((config)=>{
-            
-            wx.config({
+            var json={
                 debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: this.InstInfo.h5appid, // 必填，公众号的唯一标识
                 timestamp: config.timestamp, // 必填，生成签名的时间戳
                 nonceStr: config.nonceStr, // 必填，生成签名的随机串
                 signature: config.signature,// 必填，签名，见附录1
                 jsApiList: ["onMenuShareTimeline","onMenuShareAppMessage"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-            });
-            
+            };
+            wx.config(json);
+            var that=this;
             wx.ready(function () {
                 wx.onMenuShareAppMessage({
                         title: title,
                         desc: desc,
                         link: window.location.href,
-                        imgUrl: this.uploadpath+"inst/"+ this.InstInfo.logo,
+                        imgUrl: that.uploadpath+"inst/"+ that.InstInfo.logo,
                         trigger: function (res) {
                             // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
                             //alert('用户点击发送给朋友');
@@ -393,7 +393,7 @@ export class AppBase implements OnInit {
                     wx.onMenuShareTimeline({
                         title: title,
                         link: window.location.href,
-                        imgUrl: this.uploadpath+"inst/"+ this.InstInfo.logo,
+                        imgUrl: that.uploadpath+"inst/"+ that.InstInfo.logo,
                         trigger: function (res) {
                             // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
                             //alert('用户点击分享到朋友圈');
